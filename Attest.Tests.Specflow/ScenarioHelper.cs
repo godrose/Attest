95bb35ee004d8c05dea47856e44e07041bd1cecb
godrose <godrose@gmail.com>
2015-06-09 09:10:35 +0300

    Added support for resolvable fakes inside scenario context; they may be reused in different steps during the test run.

--- Attest.Tests.Specflow/ScenarioHelper.cs
+++ Attest.Tests.Specflow/ScenarioHelper.cs
@@ -23,6 +23,58 @@ namespace Attest.Tests.Specflow
             ScenarioContext.Current.Add(RootObjectKey, rootObjectFactory.CreateRootObject());
         }
 
+        public static void Add<TItem>(TItem item) where TItem : class
+        {           
+            var typeName = GetKey<TItem>();
+            if (ScenarioContext.Current.ContainsKey(typeName))
+            {
+                ScenarioContext.Current[typeName] = item;
+            }
+            else
+            {                
+                ScenarioContext.Current.Add(typeName, item);
+            }
+        }
+
+        public static TItem Get<TItem>() where TItem : class
+        {
+            var typeName = GetKey<TItem>();
+            if (ScenarioContext.Current.ContainsKey(typeName))
+            {
+                return ScenarioContext.Current[typeName] as TItem;
+            }
+            return null;
+        }
+
+        public static bool Contains<TItem>() where TItem : class
+        {
+            var typeName = GetKey<TItem>();
+            return ScenarioContext.Current.ContainsKey(typeName);
+        }
+
+        public static TItem Resolve<TItem>() where TItem : class, new()
+        {
+            TItem item;
+            if (Contains<TItem>() == false)
+            {
+                item = new TItem();
+                Add(item);
+            }
+            else
+            {
+                item = Get<TItem>();
+            }
+            return item;
+        }
+
+        private static string GetKey<TItem>() where TItem : class
+        {
+            var type = typeof (TItem);
+            var typeName = type.Name;
+            return typeName;
+        }
+
+
         internal static void Clear()
         {
             ScenarioContext.Current.Clear();
