25294a9da7cdb277e9ae9d6ba64d8513a511179d
godrose <godrose@gmail.com>
2015-09-21 17:16:13 +0300

    adding comments

--- Attest.Fake.Setup.Contracts/IAddCallback.cs
+++ Attest.Fake.Setup.Contracts/IAddCallback.cs
@@ -2,10 +2,30 @@ using System;
 
 namespace Attest.Fake.Setup.Contracts
 {
+    /// <summary>
+    /// Represents an object that allows to add callbacks to the callbacks container
+    /// </summary>
+    /// <typeparam name="TCallback">Type of callback</typeparam>
     public interface IAddCallback<TCallback>
     {
+        /// <summary>
+        /// Adds custom callback to the callbacks container
+        /// </summary>
+        /// <param name="methodCallback">Custom callback</param>
+        /// <returns>Callbacks container</returns>
         IMethodCallbacksContainer<TCallback> AddCallback(TCallback methodCallback);
+
+        /// <summary>
+        /// Adds successful completion callback to the callbacks container
+        /// </summary>
+        /// <returns>Callbacks container</returns>
         IMethodCallbacksContainer<TCallback> Complete();
+
+        /// <summary>
+        /// Adds exception throwing callback to the callbacks container
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns>Callbacks container</returns>
         IMethodCallbacksContainer<TCallback> Throw(Exception exception);
     }
 }
--- Attest.Fake.Setup.Contracts/IAddCallbackWithResult.cs
+++ Attest.Fake.Setup.Contracts/IAddCallbackWithResult.cs
@@ -2,25 +2,87 @@ using System;
 
 namespace Attest.Fake.Setup.Contracts
 {
+    /// <summary>
+    /// Represents an object that allows to add callbacks to the callbacks container
+    /// </summary>
+    /// <typeparam name="TCallback">Type of callback</typeparam>
+    /// <typeparam name="TResult">Type of return value</typeparam>
     public interface IAddCallbackWithResult<TCallback, in TResult>
     {
+        /// <summary>
+        /// Adds custom callback to the callbacks container
+        /// </summary>
+        /// <param name="methodCallback">Custom callback</param>
+        /// <returns>Callbacks container</returns>
         IMethodCallbacksContainer<TCallback> AddCallback(TCallback methodCallback);
+
+        /// <summary>
+        /// Adds successful completion callback to the callbacks container
+        /// </summary>
+        /// <param name="result">Successful completion return value</param>
+        /// <returns>Callbacks container</returns>
         IMethodCallbacksContainer<TCallback> Complete(TResult result);
+
+        /// <summary>
+        /// Adds successful completion callback to the callbacks container
+        /// </summary>
+        /// <param name="valueFunction">Successful completion return value's function</param>
+        /// <returns>Callbacks container</returns>
         IMethodCallbacksContainer<TCallback> Complete(Func<TResult> valueFunction);
+
+        /// <summary>
+        /// Adds exception throwing callback to the callbacks container
+        /// </summary>
+        /// <param name="exception">Exception to be thrown</param>
+        /// <returns>Callbacks container</returns>
         IMethodCallbacksContainer<TCallback> Throw(Exception exception);
+
+        /// <summary>
+        /// Adds never-ending callback to the callbacks container
+        /// </summary>
+        /// <returns>Callbacks container</returns>
         IMethodCallbacksContainer<TCallback> WithoutCallback();
     }
 
+    /// <summary>
+    /// Represents an object that allows to add callbacks to the callbacks container
+    /// </summary>
+    /// <typeparam name="TCallback">Type of callback</typeparam>
+    /// <typeparam name="T">Type of first parameter</typeparam>
+    /// <typeparam name="TResult">Type of return value</typeparam>
     public interface IAddCallbackWithResult<TCallback, T, in TResult>
     {
+        /// <summary>
+        /// Adds custom callback to the callbacks container
+        /// </summary>
+        /// <param name="methodCallback">Custom callback</param>
+        /// <returns>Callbacks container</returns>
         IMethodCallbacksContainer<TCallback> AddCallback(TCallback methodCallback);
 
+        /// <summary>
+        /// Adds successful completion callback to the callbacks container
+        /// </summary>
+        /// <param name="result">Successful completion return value</param>
+        /// <returns>Callbacks container</returns>
         IMethodCallbacksContainer<TCallback> Complete(TResult result);
 
-        IMethodCallbacksContainer<TCallback> Complete(Func<T, TResult> result);
+        /// <summary>
+        /// Adds successful completion callback to the callbacks container
+        /// </summary>
+        /// <param name="valueFunction">Successful completion return value's function</param>
+        IMethodCallbacksContainer<TCallback> Complete(Func<T, TResult> valueFunction);
 
+        /// <summary>
+        /// Adds exception throwing callback to the callbacks container
+        /// </summary>
+        /// <param name="exception">Exception to be thrown</param>
+        /// <returns>Callbacks container</returns>
         IMethodCallbacksContainer<TCallback> Throw(Exception exception);
 
+        /// <summary>
+        /// Adds never-ending callback to the callbacks container
+        /// </summary>
+        /// <returns>Callbacks container</returns>
         IMethodCallbacksContainer<TCallback> WithoutCallback();
     }
 
