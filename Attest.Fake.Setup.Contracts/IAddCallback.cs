3674c4deafbab25db061baab6ab66203ae635b20
godrose <godrose@gmail.com>
2016-02-09 09:39:11 +0200

    Added interfaces for callback management for method without return value.

--- Attest.Fake.Setup.Contracts/IAddCallback.cs
+++ Attest.Fake.Setup.Contracts/IAddCallback.cs
@@ -57,4 +57,70 @@ namespace Attest.Fake.Setup.Contracts
         /// <param name="callback">Successful completion callback</param>
         IMethodCallbacksContainer<TCallback> Complete(Action<T> callback);
     }
+
+    /// <summary>
+    /// Represents an object that allows to add callbacks to the callbacks container
+    /// </summary>
+    /// <typeparam name="TCallback">Type of callback</typeparam>
+    /// <typeparam name="T1">The type of the first parameter.</typeparam>    
+    /// <typeparam name="T2">The type of the second parameter.</typeparam>    
+    public interface IAddCallback<TCallback, T1, T2> : IAddCallbackShared<TCallback>
+    {
+        /// <summary>
+        /// Adds successful completion callback to the callbacks container
+        /// </summary>
+        /// <param name="callback">Successful completion callback</param>
+        IMethodCallbacksContainer<TCallback> Complete(Action<T1, T2> callback);
+    }
+
+    /// <summary>
+    /// Represents an object that allows to add callbacks to the callbacks container
+    /// </summary>
+    /// <typeparam name="TCallback">Type of callback</typeparam>
+    /// <typeparam name="T1">The type of the first parameter.</typeparam>    
+    /// <typeparam name="T2">The type of the second parameter.</typeparam>    
+    /// <typeparam name="T3">The type of the third parameter.</typeparam>    
+    public interface IAddCallback<TCallback, T1, T2, T3> : IAddCallbackShared<TCallback>
+    {
+        /// <summary>
+        /// Adds successful completion callback to the callbacks container
+        /// </summary>
+        /// <param name="callback">Successful completion callback</param>
+        IMethodCallbacksContainer<TCallback> Complete(Action<T1, T2, T3> callback);
+    }
+
+    /// <summary>
+    /// Represents an object that allows to add callbacks to the callbacks container
+    /// </summary>
+    /// <typeparam name="TCallback">Type of callback</typeparam>
+    /// <typeparam name="T1">The type of the first parameter.</typeparam>    
+    /// <typeparam name="T2">The type of the second parameter.</typeparam>    
+    /// <typeparam name="T3">The type of the third parameter.</typeparam>
+    /// <typeparam name="T4">The type of the fourth parameter.</typeparam>    
+    public interface IAddCallback<TCallback, T1, T2, T3, T4> : IAddCallbackShared<TCallback>
+    {
+        /// <summary>
+        /// Adds successful completion callback to the callbacks container
+        /// </summary>
+        /// <param name="callback">Successful completion callback</param>
+        IMethodCallbacksContainer<TCallback> Complete(Action<T1, T2, T3, T4> callback);
+    }
+
+    /// <summary>
+    /// Represents an object that allows to add callbacks to the callbacks container
+    /// </summary>
+    /// <typeparam name="TCallback">Type of callback</typeparam>
+    /// <typeparam name="T1">The type of the first parameter.</typeparam>    
+    /// <typeparam name="T2">The type of the second parameter.</typeparam>    
+    /// <typeparam name="T3">The type of the third parameter.</typeparam>
+    /// <typeparam name="T4">The type of the fourth parameter.</typeparam>    
+    /// <typeparam name="T5">The type of the fifth parameter.</typeparam>    
+    public interface IAddCallback<TCallback, T1, T2, T3, T4, T5> : IAddCallbackShared<TCallback>
+    {
+        /// <summary>
+        /// Adds successful completion callback to the callbacks container
+        /// </summary>
+        /// <param name="callback">Successful completion callback</param>
+        IMethodCallbacksContainer<TCallback> Complete(Action<T1, T2, T3, T4, T5> callback);
+    }
 }
--- Attest.Fake.Setup.Contracts/IHaveNoCallbacks.cs
+++ Attest.Fake.Setup.Contracts/IHaveNoCallbacks.cs
@@ -14,7 +14,53 @@ namespace Attest.Fake.Setup.Contracts
     /// </summary>
     /// <typeparam name="TCallback">Type of method callback.</typeparam>    
     /// <typeparam name="T">Type of parameter</typeparam>
-    public interface IHaveNoCallbacks<TCallback, T> : IAddCallbackWithResult<TCallback, T>
+    public interface IHaveNoCallbacks<TCallback, T> : IAddCallback<TCallback, T>
     {
     }
+
+    /// <summary>
+    /// Represents a callbacks container without method callbacks.
+    /// </summary>
+    /// <typeparam name="TCallback">Type of method callback.</typeparam>    
+    /// <typeparam name="T1">The type of the first parameter.</typeparam>
+    /// <typeparam name="T2">The type of the second parameter.</typeparam>
+    public interface IHaveNoCallbacks<TCallback, T1, T2> : IAddCallbackWithResult<TCallback, T1, T2>
+    {
+    }
+
+    /// <summary>
+    /// Represents a callbacks container without method callbacks.
+    /// </summary>
+    /// <typeparam name="TCallback">Type of method callback.</typeparam>    
+    /// <typeparam name="T1">The type of the first parameter.</typeparam>
+    /// <typeparam name="T2">The type of the second parameter.</typeparam>
+    /// <typeparam name="T3">The type of the third parameter.</typeparam>
+    public interface IHaveNoCallbacks<TCallback, T1, T2, T3> : IAddCallbackWithResult<TCallback, T1, T2, T3>
+    {
+    }
+
+    /// <summary>
+    /// Represents a callbacks container without method callbacks.
+    /// </summary>
+    /// <typeparam name="TCallback">Type of method callback.</typeparam>    
+    /// <typeparam name="T1">The type of the first parameter.</typeparam>
+    /// <typeparam name="T2">The type of the second parameter.</typeparam>
+    /// <typeparam name="T3">The type of the third parameter.</typeparam>
+    /// <typeparam name="T4">The type of the fourth parameter.</typeparam>    
+    public interface IHaveNoCallbacks<TCallback, T1, T2, T3, T4> : IAddCallbackWithResult<TCallback, T1, T2, T3, T4>
+    {
+    }
+
+    /// <summary>
+    /// Represents a callbacks container without method callbacks.
+    /// </summary>
+    /// <typeparam name="TCallback">Type of method callback.</typeparam>    
+    /// <typeparam name="T1">The type of the first parameter.</typeparam>
+    /// <typeparam name="T2">The type of the second parameter.</typeparam>
+    /// <typeparam name="T3">The type of the third parameter.</typeparam>
+    /// <typeparam name="T4">The type of the fourth parameter.</typeparam>
+    /// <typeparam name="T5">The type of the fifth parameter.</typeparam>
+    public interface IHaveNoCallbacks<TCallback, T1, T2, T3, T4, T5> : IAddCallbackWithResult<TCallback, T1, T2, T3, T4, T5>
+    {
+    }    
 }
