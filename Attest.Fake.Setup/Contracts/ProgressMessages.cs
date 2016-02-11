40bdf2c3465769ee46855b69e00abec454cab06c
godrose <godrose@gmail.com>
2016-02-11 13:32:24 +0200

    Moved Progress Messages to the Setup.

--- Attest.Fake.Setup/Contracts/ProgressMessages.cs
+++ Attest.Fake.Setup/Contracts/ProgressMessages.cs
@@ -78,20 +78,6 @@ namespace Attest.Fake.Setup.Contracts
     }
 
     /// <summary>
-    /// Represents an object that contains progress messages.
-    /// </summary>
-    public interface IHaveProgressMessages
-    {
-        /// <summary>
-        /// Gets the progress messages.
-        /// </summary>
-        /// <value>
-        /// The progress messages.
-        /// </value>
-        IEnumerable<object> ProgressMessages { get; }
-    }
-
-    /// <summary>
     /// Represents progress messages stream that has completed.
     /// </summary>
     /// <typeparam name="TCallback">The type of the callback.</typeparam>
