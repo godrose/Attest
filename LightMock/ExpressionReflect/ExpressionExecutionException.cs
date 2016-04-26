d1408666f7ad24bff89600261545336fa7cf71c6
godrose <godrose@gmail.com>
2016-04-26 09:16:18 +0300

    Added LightMock project. Re-compiled it to conform with Profile 111. Next step is to create and test portable Attes.Fake.LightMock project

---
+++ LightMock/ExpressionReflect/ExpressionExecutionException.cs
@@ -0,0 +1,37 @@
+﻿namespace ExpressionReflect
+{
+	using System;
+
+    /// <summary>
+    /// Represents an exception that occurs during expression execution.
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    public class ExpressionExecutionException : Exception
+	{
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionExecutionException"/> class.
+        /// </summary>
+        public ExpressionExecutionException()
+		{
+		}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionExecutionException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public ExpressionExecutionException(string message)
+			: base(message)
+		{
+		}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionExecutionException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="inner">The inner.</param>
+        public ExpressionExecutionException(string message, Exception inner)
+			: base(message, inner)
+		{
+		}
+	}
+}
---
+++ LightMock/ExpressionReflect/ExpressionExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace ExpressionReflect
+{
+	using System.Linq.Expressions;
+
+    /// <summary>
+    /// Extension methods for <see cref="Expression"/>
+    /// </summary>
+    public static class ExpressionExtensions
+	{
+        /// <summary>
+        /// Executes the specified values onto the expression.
+        /// </summary>
+        /// <param name="expression">The expression.</param>
+        /// <param name="values">The values.</param>
+        /// <returns></returns>
+        public static object Execute(this Expression expression, params object[] values)
+		{
+			ExpressionReflectionExecutor visitor = new ExpressionReflectionExecutor(expression);
+			object result = visitor.Execute(values);
+			return result;
+		}
+	}
+}
