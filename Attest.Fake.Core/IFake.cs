b1b47e37a1e5dc2bfeb707d79a5acb6b34601742
godrose <godrose@gmail.com>
2017-05-30 13:05:45 +0300

    Revert "Converted Attest.Fake.Core to .NETStandard"
    This reverts commit 876a95ad6839e9bb615f2c9a1f2753f82a9fbc1b.

---
+++ Attest.Fake.Core/IFake.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Attest.Fake.Core
+{
+    /// <summary>
+    /// Represents an abstraction for a fake object.
+    /// </summary>
+    /// <typeparam name="TFaked">Type of faked service</typeparam>
+    public interface IFake<TFaked> : IMock<TFaked> where TFaked: class
+    {        
+        /// <summary>
+        /// Sets up the fake according to the provided setup expression of fake call
+        /// </summary>
+        /// <param name="expression">Setup expression</param>
+        /// <returns>Fake callback after the setup</returns>
+        IFakeCallback Setup(Expression<Action<TFaked>> expression);
+
+        /// <summary>
+        /// Sets up the fake according to the provided setup expression of fake call with return value
+        /// </summary>
+        /// <typeparam name="TResult">Type of return value</typeparam>
+        /// <param name="expression">Setup expression</param>
+        /// <returns>Fake callback after the setup</returns>
+        IFakeCallbackWithResult<TResult> Setup<TResult>(Expression<Func<TFaked, TResult>> expression);
+    }
+}
