052bb192d6c1a7e9841287f994197ec1899a2fff
Gennady Verdel <godrose@gmail.com>
2017-07-08 15:59:26 +0300

    Fixed output path for 'Release' configuration

--- Attest.Testing.Core/net45/Properties/AssemblyInfo.cs
+++ Attest.Testing.Core/net45/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Attest.Tests.Core")]
+[assembly: AssemblyTitle("Attest.Testing.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Attest.Tests.Core")]
+[assembly: AssemblyProduct("Attest.Testing.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
