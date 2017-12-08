5e89f63b19451b6168e086f4484512b5afd22b92
Gennady Verdel <godrose@gmail.com>
2017-12-08 04:32:30 +0200

    Added test for mutliple calls #19

---
+++ Attest.Fake.Setup.Tests/IPhasesProvider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Attest.Fake.Builders;
+using Attest.Fake.Core;
+using Attest.Fake.Setup.Contracts;
+
+namespace Attest.Fake.Setup.Tests
+{
+    public interface IPhasesProvider
+    {
+        Guid[] GetPhasesByGauge(Guid gaugeId);
+    }
+
+    public class PhasesProviderBuilder : FakeBuilderBase<IPhasesProvider>
+    {
+        private readonly Dictionary<Guid, List<PhaseDto>> _phases = new Dictionary<Guid, List<PhaseDto>>();
+
+        public static PhasesProviderBuilder CreateBuilder()
+        {
+            return new PhasesProviderBuilder();
+        }
+
+        public void WithPhases(Guid gaugeId, IEnumerable<PhaseDto> phases)
+        {
+            if (_phases.ContainsKey(gaugeId) == false)
+            {
+                _phases.Add(gaugeId, new List<PhaseDto>());
+            }
+            _phases[gaugeId].AddRange(phases);
+        }
+
+        private IHaveNoMethods<IPhasesProvider> CreateInitialSetup()
+        {
+            return ServiceCallFactory.CreateServiceCall(FakeService);
+        }
+
+        protected override void SetupFake()
+        {
+            var initialSetup = CreateInitialSetup();
+            var setup = initialSetup.AddMethodCallWithResult<Guid, Guid[]>(
+                t => t.GetPhasesByGauge(It.IsAny<Guid>()),
+                (r, id) =>
+                    r.Complete(
+                        k => _phases[k].Select(t => t.Id).ToArray()));
+            setup.Build();
+        }
+    }
+
+    public class PhaseDto
+    {
+        public Guid GaugesId { get; set; }
+        public Guid Id { get; set; }
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public double Value { get; set; }
+    }
+}
---
+++ Attest.Fake.Setup.Tests/MultipleCallsTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Attest.Fake.Core;
+using Attest.Fake.Moq;
+using NUnit.Framework;
+using Solid.Patterns.Builder;
+
+namespace Attest.Fake.Setup.Tests
+{
+    [TestFixture]
+    public class MultipleCallsTests
+    {
+        [OneTimeSetUp]
+        public void TestFixtureSetup()
+        {
+            FakeFactoryContext.Current = new FakeFactory();
+            ConstraintFactoryContext.Current = new ConstraintFactory();
+        }
+
+        [Test]
+        public void MultipleCalls_ResultIsSetAsFunction_ResultsAreDifferent()
+        {
+            var firstGaugeId = Guid.Parse("305f5ac7-0145-4466-bd3b-d851989430f8");
+            var secondGaugeId = Guid.Parse("203a72e2-0561-4c4b-8131-e55bda2975e9");
+            var phases = new[]
+            {
+                new PhaseDto
+                {
+                    Id = Guid.Parse("80f35b7f-1c51-49ce-a93e-152b68b061a3"),
+                },
+                new PhaseDto
+                {
+                    Id = Guid.Parse("80f35b7f-1c51-49ce-a93e-152b68b061a2")
+                }
+            };
+            var phasesProviderBuilder = PhasesProviderBuilder.CreateBuilder();
+            phasesProviderBuilder.WithPhases(firstGaugeId, phases);
+            phasesProviderBuilder.WithPhases(secondGaugeId, new PhaseDto[] { });
+
+            var provider = ((IBuilder<IPhasesProvider>)phasesProviderBuilder).Build();
+            var firstPhases = provider.GetPhasesByGauge(firstGaugeId);
+            var secondPhases = provider.GetPhasesByGauge(secondGaugeId);
+            CollectionAssert.AreNotEquivalent(firstPhases, secondPhases);
+        }
+    }
+}
