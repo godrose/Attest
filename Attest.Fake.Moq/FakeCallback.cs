db5a7e3e46d33659f2083b64ace1e854b36f175a
Gennady Verdel <godrose@gmail.com>
2018-11-10 16:15:31 +0200

    #26 - Dropped .NETStandard20 suffix from Attest.Fake.Moq

---
+++ Attest.Fake.Moq/FakeCallback.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Attest.Fake.Core;
+using Moq.Language.Flow;
+
+namespace Attest.Fake.Moq
+{
+    /// <summary>
+    /// Implementation of fake callback without return value using Moq framework
+    /// </summary>
+    /// <typeparam name="TFake">Type of faked service</typeparam>
+    class MoqFakeCallback<TFake> : IFakeCallback where TFake : class
+    {
+        private readonly ISetup<TFake> _fakeSetup;
+
+        public MoqFakeCallback(ISetup<TFake> fakeSetup)
+        {
+            _fakeSetup = fakeSetup;
+        }
+
+        public void Callback(Action action)
+        {
+            _fakeSetup.Callback(action);
+        }
+
+        public void Callback<T>(Action<T> action)
+        {
+            _fakeSetup.Callback(action);
+        }
+
+        public void Callback<T1, T2>(Action<T1, T2> action)
+        {
+            _fakeSetup.Callback(action);
+        }
+
+        public void Callback<T1, T2, T3>(Action<T1, T2, T3> action)
+        {
+            _fakeSetup.Callback(action);
+        }
+
+        public void Callback<T1, T2, T3, T4>(Action<T1, T2, T3, T4> action)
+        {
+            _fakeSetup.Callback(action);
+        }
+
+        public void Callback<T1, T2, T3, T4, T5>(Action<T1, T2, T3, T4, T5> action)
+        {
+            _fakeSetup.Callback(action);
+        }
+    }
+
+    /// <summary>
+    /// Implementation of fake callback with return value using Moq framework
+    /// </summary>
+    /// <typeparam name="TFake">Type of faked service</typeparam>
+    /// <typeparam name="TResult">Type of return value</typeparam>
+    class MoqFakeCallbackWithResult<TFake, TResult> : IFakeCallbackWithResult<TResult> where TFake : class
+    {
+        private readonly ISetup<TFake, TResult> _fakeSetup;
+
+        public MoqFakeCallbackWithResult(ISetup<TFake, TResult> fakeSetup)
+        {
+            _fakeSetup = fakeSetup;
+        }
+
+        public void Callback(Func<TResult> valueFunction)
+        {
+            _fakeSetup.Returns(valueFunction);
+        }
+
+        public void Callback<T>(Func<T, TResult> valueFunction)
+        {
+            _fakeSetup.Returns(valueFunction);
+        }
+
+        public void Callback<T1, T2>(Func<T1, T2, TResult> valueFunction)
+        {
+            _fakeSetup.Returns(valueFunction);
+        }
+
+        public void Callback<T1, T2, T3>(Func<T1, T2, T3, TResult> valueFunction)
+        {
+            _fakeSetup.Returns(valueFunction);
+        }
+
+        public void Callback<T1, T2, T3, T4>(Func<T1, T2, T3, T4, TResult> valueFunction)
+        {
+            _fakeSetup.Returns(valueFunction);
+        }
+
+        public void Callback<T1, T2, T3, T4, T5>(Func<T1, T2, T3, T4, T5, TResult> valueFunction)
+        {
+            _fakeSetup.Returns(valueFunction);
+        }
+    }
+}
---
+++ Attest.Fake.Moq/FakeFactory.cs
@@ -0,0 +1,36 @@
+﻿using Attest.Fake.Core;
+using Moq;
+
+namespace Attest.Fake.Moq
+{
+    /// <summary>
+    /// Implementation of fake factory using Moq framework
+    /// </summary>    
+    public class FakeFactory : IFakeFactory
+    {
+        /// <summary>
+        /// Returns an instance of fake
+        /// </summary>
+        /// <typeparam name="TFaked">Type of fake</typeparam>
+        /// <returns>Fake instance</returns>
+        public IFake<TFaked> CreateFake<TFaked>() where TFaked : class
+        {
+            return CreateFakeImpl<TFaked>();
+        }
+
+        /// <summary>
+        /// Return an instance of mock
+        /// </summary>
+        /// <typeparam name="TFaked">Type of mock</typeparam>
+        /// <returns>Mock instance</returns>
+        public Core.IMock<TFaked> CreateMock<TFaked>() where TFaked : class
+        {
+            return CreateFakeImpl<TFaked>();
+        }
+
+        private static IFake<TFaked> CreateFakeImpl<TFaked>() where TFaked : class
+        {
+            return new Fake<TFaked>(new Mock<TFaked>(MockBehavior.Default));
+        }
+    }
+}
