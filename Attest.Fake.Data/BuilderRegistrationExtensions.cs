39f9c2f3a6e31dc30f90b8dd46053e93f6d6bfac
Gennady Verdel <godrose@gmail.com>
2018-12-12 13:20:29 +0200

    Removed unused methods

--- Attest.Fake.Data/BuilderRegistrationExtensions.cs
+++ Attest.Fake.Data/BuilderRegistrationExtensions.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Attest.Fake.Registration;
 using Solid.Patterns.Builder;
-using Solid.Practices.IoC;
 
 namespace Attest.Fake.Data
 {
@@ -11,45 +9,7 @@ namespace Attest.Fake.Data
     /// Builder registration extension methods.
     /// </summary>
     public static class BuilderRegistrationExtensions
-    {
-        /// <summary>
-        /// Registers builders products for the provided types and <see cref="IDependencyRegistrator"/> dependency registrator
-        /// from the <see cref="BuildersCollectionContext"/> 
-        /// into the provided registrator.
-        /// </summary>
-        /// <param name="dependencyRegistrator">The dependency registrator.</param>
-        /// <param name="contractsToBuildersMap">The mapping between contracts and their builders.</param>
-        /// <param name="builderFactory">The builder factory method.</param>
-        public static void RegisterBuildersProducts(
-            this IDependencyRegistrator dependencyRegistrator,
-            Dictionary<Type, Type> contractsToBuildersMap,
-            Func<Type, object> builderFactory)
-        {
-            RegisterBuilders(dependencyRegistrator,
-                RegistrationHelper.RegisterBuilderProduct
-                , contractsToBuildersMap,
-                builderFactory);
-        }
-
-        /// <summary>
-        /// Registers builders products for the provided types and <see cref="IDependencyRegistrator"/> dependency registrator
-        /// from the <see cref="BuildersCollectionContext"/> 
-        /// into the provided registrator.
-        /// </summary>
-        /// <param name="dependencyRegistrator">The dependency registrator.</param>
-        /// <param name="contractsToBuildersMap">The mapping between contracts and their builders.</param>        
-        /// <param name="builders">The builders.</param>
-        public static void RegisterBuildersProducts(
-            this IDependencyRegistrator dependencyRegistrator,
-            Dictionary<Type, Type> contractsToBuildersMap,            
-            IBuilder[] builders)
-        {
-            RegisterBuilders(dependencyRegistrator,
-                RegistrationHelper.RegisterBuilderProduct
-                , contractsToBuildersMap,                
-                builders);
-        }
-
+    {        
         /// <summary>
         /// Registers builders for the provided types and custom dependency registrator.
         /// </summary>
