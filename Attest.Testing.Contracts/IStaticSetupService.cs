4e88b342e481c2230153eedb99bd842c98005835
Gennady Verdel <godrose@gmail.com>
2019-01-15 10:00:15 +0200

    Minor

--- Attest.Testing.Contracts/IStaticSetupService.cs
+++ Attest.Testing.Contracts/IStaticSetupService.cs
@@ -2,8 +2,16 @@ using System.Collections.Generic;
 
 namespace Attest.Testing.Contracts
 {
+    /// <summary>
+    /// Represents static setup functionality
+    /// </summary>
     public interface IStaticSetupService
     {
+        /// <summary>
+        /// Invokes one-time static setup functionality and initializes
+        /// the static application modules
+        /// </summary>
+        /// <returns></returns>
         Dictionary<int, IStaticApplicationModule> OneTimeSetup();
     }
 }
