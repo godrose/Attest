e77e4c8a2760c47452644a1a12c16ea93ea642d9
Gennady Verdel <godrose@gmail.com>
2019-05-03 11:15:25 +0300

    refactor: using the base boostrapper for testing bootstrapper #25

--- Attest.Testing.Core/BootstrapperBase.cs
+++ Attest.Testing.Core/BootstrapperBase.cs
@@ -1,14 +1,5 @@
-using System.Collections.Generic;
-using System.Reflection;
 using Solid.Bootstrapping;
-using Solid.Common;
-using Solid.Core;
-using Solid.Extensibility;
-using Solid.Practices.Composition;
-using Solid.Practices.Composition.Contracts;
 using Solid.Practices.IoC;
-using Solid.Practices.Middleware;
-using Solid.Practices.Modularity;
 
 // ReSharper disable once CheckNamespace
 namespace Attest.Testing.Bootstrapping
@@ -17,73 +8,25 @@ namespace Attest.Testing.Bootstrapping
     /// Represents base bootstrapper for testing
     /// </summary>
     public abstract class BootstrapperBase :
-        IInitializable,
-        IExtensible<BootstrapperBase>,
-        IHaveAspects<BootstrapperBase>,
-        ICompositionModulesProvider,
-        IAssemblySourceProvider,
+        Solid.Bootstrapping.BootstrapperBase,       
         IHaveRegistrator
-    {
-        private ModularityAspect _modularityAspect;
-        private DiscoveryAspect _discoveryAspect;
-        private readonly ExtensibilityAspect<BootstrapperBase> _concreteExtensibilityAspect;
-        private readonly AspectsWrapper _aspectsWrapper = new AspectsWrapper();
+    {        
+        //private readonly ExtensibilityAspect<BootstrapperBase> _concreteExtensibilityAspect;        
 
         /// <summary>
         /// Creates an instance of <see cref="BootstrapperBase"/>
         /// </summary>
         /// <param name="dependencyRegistrator">The dependency registrator</param>
-        protected BootstrapperBase(IDependencyRegistrator dependencyRegistrator) : this() =>
+        protected BootstrapperBase(IDependencyRegistrator dependencyRegistrator) =>
             Registrator = dependencyRegistrator;
 
         /// <inheritdoc />
         public IDependencyRegistrator Registrator { get; }
 
-        private BootstrapperBase()
-        {
-            _concreteExtensibilityAspect = new ExtensibilityAspect<BootstrapperBase>(this);
-        }
-
-        IEnumerable<ICompositionModule> ICompositionModulesProvider<ICompositionModule>.Modules =>
-            _modularityAspect.Modules;
-
-        /// <inheritdoc />
-        public BootstrapperBase Use(IMiddleware<BootstrapperBase> middleware)
-        {
-            return _concreteExtensibilityAspect.Use(middleware);
-        }
-
-        /// <summary>
-        /// Override this to provide custom composition options
-        /// </summary>
-        public virtual CompositionOptions CompositionOptions => new CompositionOptions();
-
-        /// <inheritdoc />
-        public IEnumerable<Assembly> Assemblies => _discoveryAspect.Assemblies;
-
-        /// <inheritdoc />
-        public void Initialize()
-        {
-            _aspectsWrapper.UseCoreAspects(CreateCoreAspects());
-            _aspectsWrapper.Initialize();
-        }
-
-        private IEnumerable<IAspect> CreateCoreAspects()
-        {
-            var aspects = new List<IAspect> { new PlatformAspect() };
-            _discoveryAspect = new DiscoveryAspect(CompositionOptions);
-            _modularityAspect = new ModularityAspect(_discoveryAspect, CompositionOptions);
-            aspects.Add(_discoveryAspect);
-            aspects.Add(_modularityAspect);
-            aspects.Add(_concreteExtensibilityAspect);
-            return aspects;
-        }
-
-        /// <inheritdoc />
-        public BootstrapperBase UseAspect(IAspect aspect)
-        {
-            _aspectsWrapper.UseAspect(aspect);
-            return this;
-        }        
-    }
+        //private BootstrapperBase()
+        //{
+        //    _concreteExtensibilityAspect = new ExtensibilityAspect<BootstrapperBase>(this);
+        //}
+                
+    }    
 }
