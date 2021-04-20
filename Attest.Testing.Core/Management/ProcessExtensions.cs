b9eaa33d5ad006fdc06d1e1b068ff44ecb4d7a36
Gennady Verdel <godrose@gmail.com>
2021-04-20 18:48:27 +0300

    feat: added process management service #53

---
+++ Attest.Testing.Core/Management/ProcessExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Management;
+
+// ReSharper disable once CheckNamespace
+namespace Attest.Testing.Management
+{
+    internal static class ProcessExtensions
+    {
+        internal static void KillProcessAndChildren(this Process process)
+        {
+            KillProcessAndChildrenImpl(process?.Id ?? 0);
+        }
+
+        internal static void KillProcessAndChildren(this int pid)
+        {
+            KillProcessAndChildrenImpl(pid);
+        }
+
+        private static void KillProcessAndChildrenImpl(int pid)
+        {
+            // Cannot close 'system idle process'.
+            if (pid == 0)
+            {
+                return;
+            }
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher
+                ("Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection moc = searcher.Get();
+            foreach (ManagementObject mo in moc)
+            {
+                KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
+            }
+
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                proc.Kill();
+            }
+            catch (ArgumentException)
+            {
+                // Process already exited.
+            }
+            catch (Win32Exception)
+            {
+                // TODO: Handle Access is denied case
+            }
+        }
+    }
+}
---
+++ Attest.Testing.Core/Management/WindowsProcessManagementService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Attest.Testing.Contracts;
+
+// ReSharper disable once CheckNamespace
+namespace Attest.Testing.Management
+{
+    /// <summary>
+    /// Represents means for starting and stopping Windows OS processes.
+    /// </summary>
+    public class WindowsProcessManagementService : ProcessManagementServiceBase, IProcessManagementService
+    {
+        /// <inheritdoc />
+        public override int Start(string tool, string args)
+        {
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    UseShellExecute = true,
+                    RedirectStandardOutput = false,
+                    RedirectStandardError = false,
+                    FileName = "cmd.exe",
+                    CreateNoWindow = false,
+                    Arguments = $"/k {tool} {args}"
+                }
+            };
+            process.Start();
+            return process.Id;
+        }
+
+        /// <inheritdoc />
+        protected override Action GetStopAction(int processId)
+        {
+            return () => processId.KillProcessAndChildren();
+        }
+    }
+}
