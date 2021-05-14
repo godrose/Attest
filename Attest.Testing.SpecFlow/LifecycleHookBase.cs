bc961f028810624aef6fc4ebf9b634a9c1f07b60
Gennady Verdel <godrose@gmail.com>
2021-05-14 18:13:15 +0300

    feat(lifecycle-hook-base): added base classes for lifecycle hook and startup #56

---
+++ Attest.Testing.SpecFlow/LifecycleHookBase.cs
@@ -0,0 +1,63 @@
+﻿using Attest.Fake.Data;
+using Attest.Testing.Contracts;
+using Attest.Testing.Core;
+using BoDi;
+using Solid.Common;
+using Solid.IoC.Adapters.BoDi;
+using Solid.Practices.IoC;
+using TechTalk.SpecFlow;
+
+namespace Attest.Testing.SpecFlow
+{
+    public abstract class LifecycleHookBase
+    {
+        private readonly ObjectContainerAdapter _iocContainer;
+        private static ILifecycleService _lifecycleService;
+
+        protected LifecycleHookBase(ObjectContainer objectContainer) =>
+            _iocContainer = new ObjectContainerAdapter(objectContainer);
+
+        [BeforeTestRun]
+        public static void BeforeAllScenarios()
+        {
+            PlatformProvider.Current = new NetStandardPlatformProvider();
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            InitializeContainer(_iocContainer);
+            //TODO: Should be initialized once
+            BuildersCollectionStorageContext.Current = _iocContainer.Resolve<IDataStorage<string>>();
+            _lifecycleService = _iocContainer.Resolve<ILifecycleService>();
+            _lifecycleService.Setup();
+            _iocContainer.Setup();
+            BeforeScenarioOverride(_iocContainer);
+        }
+
+        protected abstract void InitializeContainer(IIocContainer iocContainer);
+        
+        protected void BeforeScenarioOverride(IIocContainer iocContainer)
+        {
+
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            _iocContainer.Teardown();
+            AfterScenarioOverride(_iocContainer);
+        }
+
+        protected void AfterScenarioOverride(IIocContainer iocContainer)
+        {
+
+        }
+
+        [AfterTestRun]
+        public static void AfterAllScenarios()
+        {
+            _lifecycleService.Teardown();
+        }
+    }
+}
---
+++ Attest.Testing.SpecFlow/StartupBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Attest.Testing.Core;
+using Solid.Bootstrapping;
+using Solid.Core;
+using Solid.Practices.IoC;
+using BootstrapperBase = Attest.Testing.Bootstrapping.BootstrapperBase;
+
+namespace Attest.Testing.SpecFlow
+{
+    public class StartupBase<TBootstrapper> : IInitializable where TBootstrapper : BootstrapperBase
+    {
+        private readonly IIocContainer _iocContainer;
+        private readonly Func<IIocContainer, TBootstrapper> _bootstrapperCreator;
+
+        public StartupBase(
+            IIocContainer iocContainer, 
+            Func<IIocContainer, TBootstrapper> bootstrapperCreator)
+        {
+            _iocContainer = iocContainer;
+            _bootstrapperCreator = bootstrapperCreator;
+        }
+
+        public void Initialize()
+        {
+            var bootstrapper = _bootstrapperCreator(_iocContainer);
+            bootstrapper
+                .Use(new RegisterCustomCompositionModulesMiddleware<BootstrapperBase, IDependencyRegistrator>())
+                .Use(new UseLifecycleMiddleware<BootstrapperBase>())
+                .Use(new RegisterResolverMiddleware<BootstrapperBase>(_iocContainer));
+
+            InitializeOverride(bootstrapper);
+            bootstrapper.Initialize();
+        }
+
+        protected void InitializeOverride(TBootstrapper bootstrapper)
+        {
+
+        }
+    }
+}
