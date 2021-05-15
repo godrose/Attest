09ee0baafaeab7ea5ac694cf5aca8d4fded920da
Gennady Verdel <godrose@gmail.com>
2021-05-15 08:03:03 +0300

    Feature/lifecycle hook base (#57)
    * feat(lifecycle-hook-base): bump version
    
    * feat(lifecycle-hook-base): added base classes for lifecycle hook and startup #56
    
    * feat(lifecycle-hook-base): added extension points #56
    
    * chore: bump deps
    
    * feat(lifecycle-base-hook): bump SpecFlow to 3.8.7 #56
    
    * feat(lifecycle-hook-base): put extensibility into base bootstrapper #56
    
    * feat(lifecycle-hook-base): added comments #56
    
    * feat(lifecycle-hook-base): updated package release notes #56

---
+++ Attest.Testing.Core/StartupBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Solid.Bootstrapping;
+using Solid.Core;
+using Solid.Practices.IoC;
+using BootstrapperBase = Attest.Testing.Bootstrapping.BootstrapperBase;
+
+namespace Attest.Testing.Core
+{
+    /// <summary>
+    /// Base class for bootstrapper initialization.
+    /// Used to register common dependencies and start common flows.
+    /// </summary>
+    /// <typeparam name="TBootstrapper">The type of the bootstrapper.</typeparam>
+    public class StartupBase<TBootstrapper> : IInitializable where TBootstrapper : BootstrapperBase
+    {
+        private readonly IIocContainer _iocContainer;
+        private readonly Func<IIocContainer, TBootstrapper> _bootstrapperCreator;
+
+        /// <summary>
+        /// Creates an instance of <see cref="StartupBase{TBootstrapper}"/>
+        /// </summary>
+        /// <param name="iocContainer">The IoC container.</param>
+        /// <param name="bootstrapperCreator">The bootstrapper creation function.</param>
+        public StartupBase(
+            IIocContainer iocContainer, 
+            Func<IIocContainer, TBootstrapper> bootstrapperCreator)
+        {
+            _iocContainer = iocContainer;
+            _bootstrapperCreator = bootstrapperCreator;
+        }
+
+        /// <inheritdoc />
+        public void Initialize()
+        {
+            var bootstrapper = _bootstrapperCreator(_iocContainer);
+            bootstrapper
+                .Use(new RegisterCustomCompositionModulesMiddleware<BootstrapperBase, IDependencyRegistrator>())
+                .Use(new UseLifecycleMiddleware<BootstrapperBase>())
+                .Use(new RegisterResolverMiddleware<BootstrapperBase>(_iocContainer));
+
+            InitializeOverride(bootstrapper);
+            bootstrapper.Initialize();
+        }
+
+        /// <summary>
+        /// Override to inject custom behavior during initialization.
+        /// </summary>
+        /// <param name="bootstrapper">The bootstrapper.</param>
+        protected virtual void InitializeOverride(TBootstrapper bootstrapper)
+        {
+
+        }
+    }
+}
---
+++ Attest.Testing.SpecFlow/LifecycleHookBase.cs
@@ -0,0 +1,95 @@
+﻿using Attest.Fake.Data;
+using Attest.Testing.Contracts;
+using Attest.Testing.Core;
+using BoDi;
+using Solid.Common;
+using Solid.IoC.Adapters.BoDi;
+using Solid.Practices.IoC;
+using TechTalk.SpecFlow;
+
+namespace Attest.Testing.SpecFlow
+{
+    /// <summary>
+    /// Base class for lifecycle hook to be used during scenario run.
+    /// Provides common yet extensible implementations for different lifecycle parts.
+    /// </summary>
+    public abstract class LifecycleHookBase
+    {
+        private readonly ObjectContainerAdapter _iocContainer;
+        private static ILifecycleService _lifecycleService;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="LifecycleHookBase"/>
+        /// </summary>
+        /// <param name="objectContainer">The framework's IoC container.</param>
+        protected LifecycleHookBase(ObjectContainer objectContainer) =>
+            _iocContainer = new ObjectContainerAdapter(objectContainer);
+
+        /// <summary>
+        /// Runs once before all scenarios.
+        /// </summary>
+        [BeforeTestRun]
+        public static void BeforeAllScenarios()
+        {
+            PlatformProvider.Current = new NetStandardPlatformProvider();
+        }
+
+        /// <summary>
+        /// Runs before each scenario.
+        /// </summary>
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            InitializeContainer(_iocContainer);
+            //TODO: Should be initialized once
+            BuildersCollectionStorageContext.Current = _iocContainer.Resolve<IDataStorage<string>>();
+            _lifecycleService = _iocContainer.Resolve<ILifecycleService>();
+            _lifecycleService.Setup();
+            _iocContainer.Setup();
+            BeforeScenarioOverride(_iocContainer);
+        }
+
+        /// <summary>
+        /// Override to initialize the IoC container with your dependencies.
+        /// </summary>
+        /// <param name="iocContainer">The IoC container.</param>
+        protected abstract void InitializeContainer(IIocContainer iocContainer);
+        
+        /// <summary>
+        /// Override to inject custom logic during scenario setup.
+        /// </summary>
+        /// <param name="iocContainer">The IoC container.</param>
+        protected virtual void BeforeScenarioOverride(IIocContainer iocContainer)
+        {
+
+        }
+
+        /// <summary>
+        /// Runs after each scenario.
+        /// </summary>
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            _iocContainer.Teardown();
+            AfterScenarioOverride(_iocContainer);
+        }
+
+        /// <summary>
+        /// Override to inject custom logic during scenario teardown.
+        /// </summary>
+        /// <param name="iocContainer">The IoC container.</param>
+        protected virtual void AfterScenarioOverride(IIocContainer iocContainer)
+        {
+
+        }
+
+        /// <summary>
+        /// Runs once after all scenarios.
+        /// </summary>
+        [AfterTestRun]
+        public static void AfterAllScenarios()
+        {
+            _lifecycleService.Teardown();
+        }
+    }
+}
