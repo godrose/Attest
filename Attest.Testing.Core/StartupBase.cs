41944c0800b5731d9b50a8f168bb49b1bb9a5b8d
Gennady Verdel <godrose@gmail.com>
2021-05-15 07:47:20 +0300

    feat(lifecycle-hook-base): added comments #56

--- Attest.Testing.Core/StartupBase.cs
+++ Attest.Testing.Core/StartupBase.cs
@@ -6,11 +6,21 @@ using BootstrapperBase = Attest.Testing.Bootstrapping.BootstrapperBase;
 
 namespace Attest.Testing.Core
 {
+    /// <summary>
+    /// Base class for bootstrapper initialization.
+    /// Used to register common dependencies and start common flows.
+    /// </summary>
+    /// <typeparam name="TBootstrapper">The type of the bootstrapper.</typeparam>
     public class StartupBase<TBootstrapper> : IInitializable where TBootstrapper : BootstrapperBase
     {
         private readonly IIocContainer _iocContainer;
         private readonly Func<IIocContainer, TBootstrapper> _bootstrapperCreator;
 
+        /// <summary>
+        /// Creates an instance of <see cref="StartupBase{TBootstrapper}"/>
+        /// </summary>
+        /// <param name="iocContainer">The IoC container.</param>
+        /// <param name="bootstrapperCreator">The bootstrapper creation function.</param>
         public StartupBase(
             IIocContainer iocContainer, 
             Func<IIocContainer, TBootstrapper> bootstrapperCreator)
@@ -19,6 +29,7 @@ namespace Attest.Testing.Core
             _bootstrapperCreator = bootstrapperCreator;
         }
 
+        /// <inheritdoc />
         public void Initialize()
         {
             var bootstrapper = _bootstrapperCreator(_iocContainer);
@@ -31,6 +42,10 @@ namespace Attest.Testing.Core
             bootstrapper.Initialize();
         }
 
+        /// <summary>
+        /// Override to inject custom behavior during initialization.
+        /// </summary>
+        /// <param name="bootstrapper">The bootstrapper.</param>
         protected virtual void InitializeOverride(TBootstrapper bootstrapper)
         {
 
--- Attest.Testing.SpecFlow/LifecycleHookBase.cs
+++ Attest.Testing.SpecFlow/LifecycleHookBase.cs
@@ -9,20 +9,34 @@ using TechTalk.SpecFlow;
 
 namespace Attest.Testing.SpecFlow
 {
+    /// <summary>
+    /// Base class for lifecycle hook to be used during scenario run.
+    /// Provides common yet extensible implementations for different lifecycle parts.
+    /// </summary>
     public abstract class LifecycleHookBase
     {
         private readonly ObjectContainerAdapter _iocContainer;
         private static ILifecycleService _lifecycleService;
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="LifecycleHookBase"/>
+        /// </summary>
+        /// <param name="objectContainer">The framework's IoC container.</param>
         protected LifecycleHookBase(ObjectContainer objectContainer) =>
             _iocContainer = new ObjectContainerAdapter(objectContainer);
 
+        /// <summary>
+        /// Runs once before all scenarios.
+        /// </summary>
         [BeforeTestRun]
         public static void BeforeAllScenarios()
         {
             PlatformProvider.Current = new NetStandardPlatformProvider();
         }
 
+        /// <summary>
+        /// Runs before each scenario.
+        /// </summary>
         [BeforeScenario]
         public void BeforeScenario()
         {
@@ -35,13 +49,24 @@ namespace Attest.Testing.SpecFlow
             BeforeScenarioOverride(_iocContainer);
         }
 
+        /// <summary>
+        /// Override to initialize the IoC container with your dependencies.
+        /// </summary>
+        /// <param name="iocContainer">The IoC container.</param>
         protected abstract void InitializeContainer(IIocContainer iocContainer);
         
+        /// <summary>
+        /// Override to inject custom logic during scenario setup.
+        /// </summary>
+        /// <param name="iocContainer">The IoC container.</param>
         protected virtual void BeforeScenarioOverride(IIocContainer iocContainer)
         {
 
         }
 
+        /// <summary>
+        /// Runs after each scenario.
+        /// </summary>
         [AfterScenario]
         public void AfterScenario()
         {
@@ -49,11 +74,18 @@ namespace Attest.Testing.SpecFlow
             AfterScenarioOverride(_iocContainer);
         }
 
+        /// <summary>
+        /// Override to inject custom logic during scenario teardown.
+        /// </summary>
+        /// <param name="iocContainer">The IoC container.</param>
         protected virtual void AfterScenarioOverride(IIocContainer iocContainer)
         {
 
         }
 
+        /// <summary>
+        /// Runs once after all scenarios.
+        /// </summary>
         [AfterTestRun]
         public static void AfterAllScenarios()
         {
