b7373ad70dfb8136f3e9ad115371782e16ff08af
Gennady Verdel <godrose@gmail.com>
2021-05-23 07:51:48 +0300

    refactor: added framework-agnostic scenario data store base class #62

---
+++ Attest.Testing.Core/SimpleScenarioDataStore.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Attest.Testing.Core
+{
+    /// <summary>
+    /// Simple implementation of <see cref="ScenarioDataStoreBase"/> using <see cref="Dictionary{TKey,TValue}"/>.
+    /// </summary>
+    public class SimpleScenarioDataStore : ScenarioDataStoreBase
+    {
+        private IDictionary Properties { get; } = new Dictionary<string, object>();
+
+        protected override bool ContainsKey(string key)
+        {
+            return Properties.Contains(key);
+        }
+
+        protected override T GetValueByKey<T>(string key)
+        {
+            return (T) Properties[key];
+        }
+
+        protected override void SetValueByKey<T>(T value, string key)
+        {
+            Properties[key] = value;
+        }
+    }
+}
