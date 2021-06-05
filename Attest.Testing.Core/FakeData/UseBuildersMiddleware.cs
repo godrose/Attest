afd4c6963f979d461a8258f90e4a17823ffd91c8
Gennady Verdel <godrose@gmail.com>
2021-06-05 18:08:15 +0300

    feat(enhancing-middlewares): adding more middlewares for easier consumption #78

---
+++ Attest.Testing.Core/FakeData/UseBuildersMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Solid.Bootstrapping;
+using Solid.Practices.Middleware;
+
+// ReSharper disable once CheckNamespace
+namespace Attest.Testing.FakeData
+{
+    /// <summary>
+    /// This middleware registers dependencies used during integration tests.
+    /// </summary>
+    /// <typeparam name="TBootstrapper">The type of the bootstrapper.</typeparam>
+    public sealed class UseBuildersMiddleware<TBootstrapper> : IMiddleware<TBootstrapper>
+        where TBootstrapper : class, IHaveRegistrator
+    {
+        /// <inheritdoc />
+        public TBootstrapper Apply(TBootstrapper @object)
+        {
+            @object.Registrator.UseBuilders();
+            return @object;
+        }
+    }
+}
