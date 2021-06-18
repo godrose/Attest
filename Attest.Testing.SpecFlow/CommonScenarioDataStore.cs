36ba179c3ac123d77cb4e7927b1efb95a0e01d9d
Gennady Verdel <godrose@gmail.com>
2021-06-18 11:25:41 +0300

    chore: bump version

---
+++ Attest.Testing.SpecFlow/CommonScenarioDataStore.cs
@@ -0,0 +1,24 @@
+﻿using Solid.Practices.IoC;
+using TechTalk.SpecFlow;
+
+namespace Attest.Testing.SpecFlow
+{
+    public class CommonScenarioDataStore<TRootObject> : ScenarioDataStoreBase
+    {
+        public CommonScenarioDataStore(ScenarioContext scenarioContext) : base(scenarioContext)
+        {
+        }
+
+        public TRootObject RootObject
+        {
+            get => GetValue<TRootObject>();
+            set => SetValue(value);
+        }
+
+        public IIocContainer IocContainer
+        {
+            get => GetValue<IIocContainer>();
+            set => SetValue(value);
+        }
+    }
+}
