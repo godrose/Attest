21871374826c98f79112394d2abeafd84f921a61
Gennady Verdel <godrose@gmail.com>
2021-07-07 08:42:35 +0300

    feat(configuration): added support for run guard #84

---
+++ Attest.Testing.Core/Configuration/RunGuard.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+
+// ReSharper disable once CheckNamespace
+namespace Attest.Testing.Configuration
+{
+    public class RunGuard : IRunGuard
+    {
+        private readonly IConfigurationSectionValueProvider _configurationSectionValueProvider;
+        private readonly IRunGuardInfo _runGuardInfo;
+
+        public RunGuard(
+            IConfigurationSectionValueProvider configurationSectionValueProvider,
+            IRunGuardInfo runGuardInfo)
+        {
+            _configurationSectionValueProvider = configurationSectionValueProvider;
+            _runGuardInfo = runGuardInfo;
+        }
+
+        public bool CanRun(IConfiguration configuration)
+        {
+            var runGuardConfiguredValue = _configurationSectionValueProvider.GetValue(configuration, _runGuardInfo.Key);
+            var canRun = _runGuardInfo.CanRun(runGuardConfiguredValue);
+            return canRun;
+        }
+    }
+}
---
+++ Attest.Testing.Core/Configuration/RunGuardInfoBase.cs
@@ -0,0 +1,23 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Attest.Testing.Configuration
+{
+    public abstract class RunGuardInfoBase : IRunGuardInfo
+    {
+        protected RunGuardInfoBase(string key)
+        {
+            Key = key;
+        }
+
+        public string Key { get; }
+        protected abstract object TruthyValue { get; }
+        protected abstract object ParseValue(string input);
+
+        public bool CanRun(string input)
+        {
+            var parsedValue = ParseValue(input);
+            return TruthyValue == null ? 
+                parsedValue == null : 
+                parsedValue != null && parsedValue.Equals(TruthyValue);
+        }
+    }
+}
