3b4b7b0d3fbb3db1b9128b8ba4d876b8966f0cf6
Gennady Verdel <godrose@gmail.com>
2021-10-29 08:47:48 +0300

    feat(init-default): reproducing the complex type access bug

---
+++ src/Attest.Testing.SpecFlow.Specs/ComplexTypesScenarioDataStore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Attest.Testing.Context.SpecFlow;
+using TechTalk.SpecFlow;
+
+namespace Attest.Testing.SpecFlow.Specs
+{
+    internal sealed class ComplexTypesScenarioDataStore : ScenarioDataStoreBase
+    {
+        public ComplexTypesScenarioDataStore(ScenarioContext scenarioContext) : base(scenarioContext)
+        {
+        }
+
+        public Dictionary<string, string> ComplexType
+        {
+            get => GetValue<Dictionary<string, string>>();
+            set => SetValue(value);
+        }
+
+        public Exception Error
+        {
+            get => GetValue<Exception>();
+            set => SetValue(value);
+        }
+    }
+}
---
+++ src/Attest.Testing.SpecFlow.Specs/ComplexTypesSteps.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Attest.Testing.SpecFlow.Specs
+{
+    [Binding]
+    internal sealed class ComplexTypesSteps
+    {
+        private readonly ComplexTypesScenarioDataStore _scenarioDataStore;
+
+        public ComplexTypesSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioDataStore = new ComplexTypesScenarioDataStore(scenarioContext);
+        }
+
+        [Given(@"The scenario data store contains property with Dictionary type")]
+        public void GivenTheScenarioDataStoreContainsPropertyWithDictionaryType()
+        {
+            //readability only
+        }
+
+        [When(@"I add an entry to this property")]
+        public void WhenIAddAnEntryToThisProperty()
+        {
+            var error = Record.Exception(() => _scenarioDataStore.ComplexType.Add("name", "value"));
+            _scenarioDataStore.Error = error;
+        }
+
+        [Then(@"No exception is thrown")]
+        public void ThenNoExceptionIsThrown()
+        {
+            _scenarioDataStore.Error.Should().BeNull();
+        }
+    }
+}
