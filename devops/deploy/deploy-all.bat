6e10d61eda14f73a2ac09501b9fd0cfe77e1ddf3
Gennady Verdel <godrose@gmail.com>
2021-11-05 12:10:08 +0200

    devops: bump version to 2.4.2-rc1

--- devops/deploy/deploy-all.bat
+++ devops/deploy/deploy-all.bat
@@ -1,5 +1,5 @@
 rem TODO: Use common source for all version instances
-SET version=2.4.1
+SET version=2.4.2-rc1
 rem TODO: Refactor using loop and automatic discovery
 call deploy-single.bat Attest.Fake.Builders %version% 
 call deploy-single.bat Attest.Fake.Conventions %version% 
