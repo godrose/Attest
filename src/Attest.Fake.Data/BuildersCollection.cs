de176e09bdf7ad498669f03d4400534198a27be8
Gennady Verdel <godrose@gmail.com>
2022-02-01 16:46:38 +0200

    feat(builders-collection): made builders collection's methods public (#109)

--- src/Attest.Fake.Data/BuildersCollection.cs
+++ src/Attest.Fake.Data/BuildersCollection.cs
@@ -11,22 +11,40 @@ namespace Attest.Fake.Data
     {
         private readonly List<object> _allBuilders = new List<object>();
 
-        internal IEnumerable<IBuilder<TService>> GetBuilders<TService>() where TService : class
+        /// <summary>
+        /// Gets builders of the specified type.
+        /// </summary>
+        /// <typeparam name="TService">The builder type.</typeparam>
+        /// <returns></returns>
+        public IEnumerable<IBuilder<TService>> GetBuilders<TService>() where TService : class
         {
             return _allBuilders.OfType<IBuilder<TService>>();
         }
 
-        internal IEnumerable<object> GetAllBuilders()
+        /// <summary>
+        /// Gets all builders.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<object> GetAllBuilders()
         {
             return _allBuilders;
         }
 
-        internal void AddBuilder<TService>(IBuilder<TService> builder) where TService : class
+        /// <summary>
+        /// Adds builder of the specified type.
+        /// </summary>
+        /// <typeparam name="TService">The builder type.</typeparam>
+        /// <param name="builder">The builder.</param>
+        public void AddBuilder<TService>(IBuilder<TService> builder) where TService : class
         {
             _allBuilders.Add(builder);
         }
 
-        internal void ResetBuilders(IEnumerable<object> builders)
+        /// <summary>
+        /// Resets the builders collection with the provided value.
+        /// </summary>
+        /// <param name="builders">The new builders collection.</param>
+        public void ResetBuilders(IEnumerable<object> builders)
         {
             _allBuilders.Clear();
             _allBuilders.AddRange(builders);
