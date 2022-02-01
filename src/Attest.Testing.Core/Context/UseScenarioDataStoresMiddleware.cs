c10e117360073eb1260f669fc6bc022f129687f9
Gennady Verdel <godrose@gmail.com>
2022-02-01 19:27:35 +0200

    feat(data-stores): adding support during bootstrapping (#110)

---
+++ src/Attest.Testing.Core/Context/UseScenarioDataStoresMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using Attest.Testing.Bootstrapping;
+using Solid.Practices.Middleware;
+
+namespace Attest.Testing.Context
+{
+    /// <summary>
+    /// This middleware allows using scenario data stores.
+    /// </summary>
+    /// <typeparam name="TBootstrapper"></typeparam>
+    public class UseScenarioDataStoresMiddleware<TBootstrapper> : IMiddleware<TBootstrapper>
+        where TBootstrapper : BootstrapperBase
+    {
+        /// <inheritdoc />
+        public TBootstrapper Apply(TBootstrapper @object)
+        {
+            @object.Registrator.RegisterScenarioDataStoresAsContracts(@object.Assemblies);
+            return @object;
+        }
+    }
+}
