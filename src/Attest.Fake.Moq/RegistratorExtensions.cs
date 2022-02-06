91fcb29db41f6e20116e61b5359f8de8911603c6
Gennady Verdel <godrose@gmail.com>
2022-02-06 17:52:12 +0200

    feat(moq): added extensions

---
+++ src/Attest.Fake.Moq/RegistratorExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Attest.Fake.Core;
+
+namespace Attest.Fake.Moq
+{
+    /// <summary>
+    /// The registrator extensions.
+    /// </summary>
+    public static class RegistratorExtensions
+    {
+        /// <summary>
+        /// Registers Moq services as fake functionality providers.
+        /// </summary>
+        /// <typeparam name="TExtensible"></typeparam>
+        /// <param name="extensible"></param>
+        /// <returns></returns>
+        public static TExtensible UseMoq<TExtensible>(
+            this TExtensible extensible)
+            where TExtensible : class
+        {
+            FakeFactoryContext.Current = new FakeFactory();
+            ConstraintFactoryContext.Current = new ConstraintFactory();
+            return extensible;
+        }
+    }
+}
